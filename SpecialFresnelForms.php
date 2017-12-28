51f2301ffbc304fb812474e4dfa17cdada571f62
LloydRutledge <Lloyd.Rutledge@ou.nl>
2017-12-28 11:04:33 +0000

    generated page content copied direct on special page

--- SpecialFresnelForms.php
+++ SpecialFresnelForms.php
@@ -320,9 +320,11 @@ function frmRow ( $propName , $autocomp , $frmsParams ) {
 }
 
 function makePage ( $prefix , $name , $content ) {
+	global $wgOut;
 	if ( $content == '' ) $content =  '<span> </span>' ;
 	$newarticle = new Article(Title::newFromText( $prefix . ':' . $name ) , 0);
 // stop until can get working	$newarticle->doEdit( $content , EDIT_UPDATE);
+   	$wgOut->addWikiText ( "<pre><nowiki>" . $prefix . ":" . $name . " " . $content . "</nowiki></pre>" ); // page code on special page
 }
 
 function makeCategoryPage ( $catName ) {
