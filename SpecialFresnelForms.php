dee20e07532a8a29fe0888bb0363d0655753c49a
Lloyd Rutledge <Lloyd.Rutledge@ou.nl>
2018-12-29 17:03:51 +0100

    generated Form code tidied wikiPreview removed

--- SpecialFresnelForms.php
+++ SpecialFresnelForms.php
@@ -377,7 +377,6 @@ function makeFormPage ( $boxName , $FrmRows ) {
 '<noinclude>
 {{#forminput:form=' . $boxName . '}}
 </noinclude><includeonly>
-<div id="wikiPreview" style="display: none; padding-bottom: 25px; margin-bottom: 25px; border-bottom: 1px solid #AAAAAA;"></div>
 {{{for template|Informbox ' . $boxName . '|label=' . $boxName . '}}}
 {| class="formtable"
 ';
